417d4e5e0df06c4a7bca79a880e8418e3207ea68
Vladimir Dolnikov <v.dolnikov@yandex.ru>
2018-11-27 01:24:35 +0700

    Изменил название папки Yiilesson на Yii

--- public_html/index.php
+++ public_html/index.php
@@ -2,16 +2,16 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require __DIR__ . '/../yiilesson/vendor/autoload.php';
-require __DIR__ . '/../yiilesson/vendor/yiisoft/yii2/Yii.php';
-require __DIR__ . '/../yiilesson/common/config/bootstrap.php';
-require __DIR__ . '/../yiilesson/frontend/config/bootstrap.php';
+require __DIR__ . '/../yii/vendor/autoload.php';
+require __DIR__ . '/../yii/vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../yii/common/config/bootstrap.php';
+require __DIR__ . '/../yii/frontend/config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
-    require __DIR__ . '/../yiilesson/common/config/main.php',
-    require __DIR__ . '/../yiilesson/common/config/main-local.php',
-    require __DIR__ . '/../yiilesson/frontend/config/main.php',
-    require __DIR__ . '/../yiilesson/frontend/config/main-local.php'
+    require __DIR__ . '/../yii/common/config/main.php',
+    require __DIR__ . '/../yii/common/config/main-local.php',
+    require __DIR__ . '/../yii/frontend/config/main.php',
+    require __DIR__ . '/../yii/frontend/config/main-local.php'
 );
 
 (new yii\web\Application($config))->run();
